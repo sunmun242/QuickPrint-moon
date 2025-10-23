7fe9d900387f973385d4c78ef1206dd457f37698
sunmun242 <sunmunislam955@gmail.com>
2025-10-23 15:09:13 +0530

    PhonePe integration team final fix

--- app.py
+++ app.py
@@ -259,7 +259,6 @@ def payment_initiate():
                     {"type": "NET_BANKING"},
                     {"type": "CARD", "cardTypes": ["DEBIT_CARD", "CREDIT_CARD"]}
                 ],
-                "disabledPaymentModes": [] # সব মোড এনাবল রাখতে ফাঁকা রাখা হলো
             }
         }
     }
